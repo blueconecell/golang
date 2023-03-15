5f60a0c8ed31da8fa80fed97ee67053dfd4f51cb
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-03-15 15:31:52 +0900

    1.3

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+func lenAndUpper(name string) (int, string){
+	return len(name), strings.ToUpper(name)
+}
+func multiply(a,b int) int{
+	return a * b
+}
+func repeatMe(words ...string){
+	fmt.Println(words)
+
+
+}
 func main() {
-	name := "abcd"
-	fmt.Println(name)
+	totalLength, upperName := lenAndUpper("qwe3r")
+	fmt.Println(totalLength, upperName)
+
+	repeatMe("dog", "cat", "zebra", "moon")
 }
